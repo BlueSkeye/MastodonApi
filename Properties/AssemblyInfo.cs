af5a76cc00f43ecedb439728b715b8ebc3ae7a1d
BlueSkeye <bsgithub@gmail.com>
2017-04-21 15:43:44 +0200

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MastodonApi")]
+[assembly: AssemblyDescription("A C# implementation of the Mastodon API")]
+[assembly: AssemblyProduct("MastodonApi")]
+[assembly: AssemblyCopyright("MIT License")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a5887820-bd88-40ad-b869-57a50a0e542e")]
+[assembly: AssemblyVersion("1.0.*")]
