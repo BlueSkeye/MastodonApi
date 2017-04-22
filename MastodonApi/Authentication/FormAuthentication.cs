bb5acb241bb674fa20fbd23f2287cd19159cb956
BlueSkeye <bsgithub@gmail.com>
2017-04-22 15:00:46 +0200

    Login/logout scenario in progress

--- MastodonApi/Authentication/FormAuthentication.cs
+++ MastodonApi/Authentication/FormAuthentication.cs
@@ -5,6 +5,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
+using MastodonApi.Logging;
+
 namespace MastodonApi.Authentication
 {
     /// <summary></summary>
@@ -19,31 +21,65 @@ namespace MastodonApi.Authentication
         public async Task<string> Connect(string serverName)
         {
             HttpClientHandler clientHandler = new HttpClientHandler();
-            HttpClient client = new HttpClient(clientHandler);
+            ConsoleLoggingHandler logger = new ConsoleLoggingHandler(clientHandler);
+            HttpClient client = new HttpClient(logger);
             client.BaseAddress = new Uri(string.Format(ConnectionUrlBasePattern, serverName));
             CookieCollection cookies = null;
             string authenticityToken = null;
+            string mastodonSession = null;
             try {
                 HttpResponseMessage response = await client.GetAsync(ConnectionUrlPath);
                 if (response.IsSuccessStatusCode) {
                     cookies = clientHandler.CookieContainer.GetCookies(client.BaseAddress);
+                    mastodonSession= cookies["_mastodon_session"].Value;
                     HttpResponseHeaders headers = response.Headers;
                     authenticityToken = await RetrieveAuthentivityToken(response);
                 }
+                // cookies.Add(new Cookie("_mastodon_session", authenticityToken));
                 NetworkCredential credentials = _credentialProvider();
                 if (null == credentials) {
                     throw new ApplicationException();
                 }
                 List<KeyValuePair<string, string>> postData = new List<KeyValuePair<string, string>>();
-                postData.Add(new KeyValuePair<string, string>("utf8", "%E2%9C%93"));
+                postData.Add(new KeyValuePair<string, string>("utf8", "✓"));
                 postData.Add(new KeyValuePair<string, string>("authenticity_token", authenticityToken));
-                postData.Add(new KeyValuePair<string, string>("user%5Bemail%5D",
+                postData.Add(new KeyValuePair<string, string>("user[email]",
                     WebUtility.UrlEncode(credentials.UserName)));
-                postData.Add(new KeyValuePair<string, string>("user%5Bpassword%5D",
+                postData.Add(new KeyValuePair<string, string>("user[password]",
                     WebUtility.UrlEncode(credentials.Password)));
                 postData.Add(new KeyValuePair<string, string>("button", ""));
                 FormUrlEncodedContent postContent = new FormUrlEncodedContent(postData);
+                logger.LoggingEnabled = true;
+                HttpRequestHeaders postHeaders = client.DefaultRequestHeaders;
+                postHeaders.Accept.Clear();
+                postHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+                postHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xhtml+xml"));
+                postHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml", 0.9));
+                postHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("image/webp"));
+                postHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*",0.8));
+                postHeaders.AcceptEncoding.Clear();
+                //postHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+                //postHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+                //postHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("br"));
+                postHeaders.AcceptLanguage.Clear();
+                postHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("fr-FR"));
+                postHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("fr", 0.8));
+                postHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en-US", 0.6));
+                postHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("en", 0.4));
+                postHeaders.CacheControl = new CacheControlHeaderValue() { MaxAge = new TimeSpan() };
+                postHeaders.Connection.Clear();
+                postHeaders.Connection.Add("keep-alive");
+                //Cookie:__cfduid=d59eff9cf3648d1c2347c5f94ae7f1a651492778490; _mastodon_session=Mi9JVjVBRnhUZTRiRW1iM1VQczVycFVOL2Y5MzNpL3FmSm92blRiSWJScy94QmdlR1JzMHVvN0p5NTZUcmRURzVFc1R1QTNWY3lldW5hRVU1NW9Ca3U1R2VDMngvUUlyYkQ3Y2dQRWdFRjlQL1drV2g2aG5CZDVvRjJCc2xnZk5IQmg1STdYNUNuZGgxOTRMTjVMeUxjYzQzbVBGK0NHOXBEZWRnSmdpL2g5VUpIYTlJMHJ5eDM0N3psZU12dXBaLS1aVFAvalV6UE5mdE16WUZ2UldpU29nPT0%3D--f5ce9c8122b54e0633705d143856fa1f4ddc227c
+                //Host:mastodon.social
+                //Origin:https://mastodon.social
+                //Referer:https://mastodon.social/auth/sign_in
+                postHeaders.Add("Upgrade-Insecure-Requests", "1");
+                //User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36
+                // TODO : postHeaders.Referrer
+                postHeaders.Add("Cookie", string.Format("_mastodon_session={0}", mastodonSession));
                 response = await client.PostAsync(ConnectionUrlPath, postContent);
+                string rawTextContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(rawTextContent);
                 if (response.IsSuccessStatusCode) {
                     int i = 1;
                 }
---
+++ MastodonApi/Logging/ConsoleLoggingHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MastodonApi.Logging
+{
+    public class ConsoleLoggingHandler : DelegatingHandler
+    {
+        public ConsoleLoggingHandler(HttpClientHandler innerHandler)
+            : base(innerHandler)
+        {
+            return;
+        }
+
+        public bool LoggingEnabled { get; set; }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (LoggingEnabled) {
+                Console.WriteLine("Request:");
+                Console.WriteLine(request.ToString());
+                if (null != request.Content) {
+                    Console.WriteLine(await request.Content.ReadAsStringAsync());
+                }
+                Console.WriteLine();
+            }
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+            if (LoggingEnabled) {
+                Console.WriteLine("Response:");
+                Console.WriteLine(response.ToString());
+                if (null != response.Content) {
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
+                }
+                Console.WriteLine();
+            }
+            return response;
+        }
+    }
+}
